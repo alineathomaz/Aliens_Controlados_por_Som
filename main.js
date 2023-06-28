cec405189def1c57ad2e70915b6d500666b234b5
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-06-27 21:40:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -24,8 +24,8 @@ function gotResults(error, results)
 
     document.getElementById("result_label").innerHTML = 'Posso ouvir: '+ results[0].label;
     document.getElementById("result_confidence").innerHTML = 'Precisão: '+ (results[0].confidence*100).toFixed(2)+" %";
-    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
-    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
 
     img = document.getElementById('alien1') 
     img1 = document.getElementById('alien2')
